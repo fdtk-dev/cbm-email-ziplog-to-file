710e1be57c1fcca9bf7bb395f5777396c288e97d
ceds_log@fw.freedom-tek.xom <marty.chao@gmail.com>
2024-05-06 20:50:04 +0800

    optimaze

--- trigger.sh
+++ trigger.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 filename=$1
 today=Today:${filename:4:8}
-work_dir=/home/ceds_log/cbm_log
-base_dir=/home/ceds_log/cbm_zip
-python_bin=/home/ceds_log/miniforge3/envs/procmail/bin/python
-unzip -o -d $work_dir $base_dir/$filename
-/usr/bin/rclone copy --max-age 12h $work_dir gd:/05.其他案/高鐵CBM_PCDL-23-0315/cbm-log
+file_ext=${filename:0-3}
+work_dir=~/cbm_log
+base_dir=~/cbm_zip
+python_bin=~/miniforge3/envs/procmail/bin/python
+if [ $file_ext = "zip" ]; then
+	unzip -o -d $work_dir $base_dir/$filename
+	/usr/bin/rclone copy --max-age 12h $work_dir gd:/05.其他案/高鐵CBM_PCDL-23-0315/cbm-log
+	source ~/github/cbm-email-ziplog-to-file/token.ini
+	message=$(date +%F)$(echo -e '\n')$(~/github/cbm-email-ziplog-to-file/m10-1-check.sh)
+	$python_bin /home/ceds_log/github/notify/notify_line.py "$message"
+fi
