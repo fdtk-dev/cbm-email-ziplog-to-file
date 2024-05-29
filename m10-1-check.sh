cc1c300b01d025f0512ea4bd6d100dcd7ae91f1d
ceds_log@fw.freedom-tek.xom <marty.chao@gmail.com>
2024-05-29 08:33:51 +0800

    add psu detial message

--- m10-1-check.sh
+++ m10-1-check.sh
@@ -10,12 +10,17 @@ nic=`cat $LOG_FILE |grep -e "^net[0-9]" |grep ok |wc -l`
 dbs=`cat $LOG_FILE |grep "COUNT(" -A 3 |grep "   "|wc -l `
 zpool=`cat $LOG_FILE |grep -E "(rpool|backup).*[1-9]* -" |grep ONLINE |wc -l`
 nxport=`cat $LOG_FILE |grep ./nxinit |wc -l`
+echo
 if [ $hdd = "153" ]; then echo "硬碟正常 153顆" ; else echo "硬碟異常" ; fi
 if [ $fan = "301" ]; then echo "風扇正常 301顆" ; else echo "風扇異常" ; fi
-if [ $psu = "86" ]; then echo "電源供應器正常 86顆" ; else echo "電源供應器異常" ; fi
+if [ $psu = "86" ]; then echo "電源供應器正常 86顆" ; 
+else 
+	echo "電源供應器異常" ; 
+	cat $LOG_FILE |grep -E "### |PSU#|<div hostname" |grep -v Normal|grep PSU -B 2
+fi
 if [ $cpu = "43" ]; then echo "CPU正常 43顆" ; else echo "CPU異常" ; fi
-if [ $ram = "176" ]; then echo "RAM正常 176顆" ; else echo "RAM異常" ; fi
-if [ $nic = "237" ]; then echo "NET#正常 237顆" ; else echo "NET#異常" ; fi
+if [ $ram = "176" ]; then echo "RAM正常 176條" ; else echo "RAM異常" ; fi
+if [ $nic = "237" ]; then echo "NET#正常 237個" ; else echo "NET#異常" ; fi
 if [ $dbs = "16" ]; then echo "Oracle 資料庫正常 16個" ; else echo "Oracle 資料庫異常" ; fi
 if [ $zpool = "68" ]; then echo "zpool 資料正常 68個" ; else echo "zpool 資料異常" ; fi
 if [ $nxport = "43" ]; then echo "nxport正常 43臺" ; else echo "nxport異常" ; fi
