615de930b16980f0fc8ad2e40ef0e626c4ad5189
ceds_log@fw.freedom-tek.xom <marty.chao@gmail.com>
2024-06-27 08:35:56 +0800

    fix: rclone copy to google drive not working

--- trigger.sh
+++ trigger.sh
@@ -5,11 +5,12 @@ file_ext=${filename:0-3}
 work_dir=~/cbm_log
 base_dir=~/cbm_zip
 python_bin=~/miniforge3/envs/procmail/bin/python
-if [ $file_ext = "zip" ]; then
+if [ "$file_ext" == "zip" ]; then
 	unzip -o -d $work_dir $base_dir/$filename
 	/usr/bin/rclone copy --max-age 12h $work_dir gd:/05.其他案/高鐵CBM_PCDL-23-0315/cbm-log
 	source ~/github/cbm-email-ziplog-to-file/token.ini
 	message=$(date +%F)$(echo -e '\n')$(~/github/cbm-email-ziplog-to-file/m10-1-check.sh)
 	$python_bin /home/ceds_log/github/notify/notify_line.py "$message"
+	$python_bin /home/ceds_log/github/cbm-email-ziplog-to-file/read_log2json.py today  > /tmp/trigger_status_csv.log
+	/usr/bin/rclone copy --max-age 12h ~/cbm_log_csv gd:/05.其他案/高鐵CBM_PCDL-23-0315/cbm-log
 fi
-$python_bin /home/ceds_log/github/cbm-email-ziplog-to-file/read_log2json.py today > /dev/null
